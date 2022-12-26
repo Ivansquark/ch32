e2a0151e0c6be10ff737b1ee904dd4b3c061ffeb
Ivansquark <squark@mail.ru>
2022-12-26 15:45:10 +0300

    try to load

--- ch32v307/tut1/src/main.c
+++ ch32v307/tut1/src/main.c
@@ -1,23 +1,25 @@
-void GPIO_Toggle_INIT(void)
-{
-    GPIO_InitTypeDef GPIO_InitStructure = {0};
+#include "main.h"
+// void GPIO_Toggle_INIT(void)
+//{
+//    GPIO_InitTypeDef GPIO_InitStructure = {0};
+//
+//    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+//    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+//    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+//    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+//    GPIO_Init(GPIOA, &GPIO_InitStructure);
+//}
 
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_Init(GPIOA, &GPIO_InitStructure);
-}
+int main(void) {
+    uint8_t i = 0;
+    int y = 0;
+    // NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+    // GPIO_Toggle_INIT();
 
-int main(void)
-{
-    u8 i = 0;
-
-    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
-    GPIO_Toggle_INIT();
-
-    while(1)
-    {
-        GPIO_WriteBit(GPIOA, GPIO_Pin_0, (i == 0) ? (i = Bit_SET) : (i = Bit_RESET));
+    while (1) {
+        // GPIO_WriteBit(GPIOA, GPIO_Pin_0, (i == 0) ? (i = Bit_SET) : (i =
+        // Bit_RESET));
+        i++;
+        y = i;
     }
 }
